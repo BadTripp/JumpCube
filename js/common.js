9a2ded4c759aa27d0ed5d9bb5552add185a20bf4
BadTripp <renato.turetta00@gmail.com>
2022-03-24 14:52:31 +0100

    Core html-css

--- js/common.js
+++ js/common.js
@@ -6,17 +6,21 @@ lar_sprite=quadrato.offsetLeft;
 barraSprite=document.getElementById("barra");
 step=0;
 step=schermox+30;
-jumprange=50;
+jumprange=80;
 jumpspeed=1;
+altezzabase=180;
 function movimento(){
     quadrato=document.getElementById("quadrato");
     alt_sprite=quadrato.offsetTop;
     lar_sprite=quadrato.offsetLeft;
+    lar_barra=barraSprite.offsetLeft;
+    alt_barra=barraSprite.offsetTop;
     deltaTop=alt_sprite-schermoy;
     deltaLeft=lar_sprite-schermox;
     //console.log("|MonitorX:"+schermox+"|MonitorY:"+schermoy+"|XSprite:"+lar_sprite,"|YSprite:"+alt_sprite);
-    
+    console.log(deltaTop);
     document.addEventListener("keydown", comandi);
+    if(lar_sprite == lar_barra && alt_sprite == alt_barra ){alert("Game Over!")}
     barra();
 }
 function barra(){
@@ -32,23 +36,27 @@ function barra(){
 }
 function comandi(event){
     var keyID = event.keyCode;
-    
     c=0;
     if(keyID == 32){
-        
-           Isalto= setInterval(salto,90);
-           console.log(alt_sprite);
-            
-            
-        
-        
+        Isalto=setInterval(salto,13); 
     }
 
 }
-function salto(){
-    jumpspeed+=1;
-    jumpspeed=alt_sprite+jumpspeed
-    console.log(jumpspeed);
-            quadrato.style.top=+"rx";
-    if(alt_sprite <= 188+50){clearInterval(Isalto);}
+function salto(){   
+    jumpspeed=alt_sprite;
+    jumpspeed-=4;
+    quadrato.style.top=jumpspeed+"px";  
+    console.log(alt_sprite,jumprange,altezzabase,altezzabase-jumprange)
+
+    if(alt_sprite <= altezzabase-jumprange){
+        clearInterval(Isalto);
+        Ibase=setInterval(base,18)
+    }
+}
+function base(){
+            jumpspeed=alt_sprite;
+            jumpspeed+=4;
+            quadrato.style.top=jumpspeed+"px";  
+            console.log(alt_sprite,jumprange,altezzabase,altezzabase-jumprange)
+            if(alt_sprite == 180){clearInterval(Ibase);}
 }
