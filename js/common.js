0245b6f94d2480fcdb12967cf1daa10b46603d9e
BadTripp <renato.turetta00@gmail.com>
2022-03-24 16:25:24 +0100

    Core js complete
    Aggiunte collissione e icona cuore nel quadrato

--- js/common.js
+++ js/common.js
@@ -1,34 +1,46 @@
-setInterval(movimento,5)
+gameengine=setInterval(movimento,5)
 schermoy=document.getElementById("schermo").clientHeight;
 schermox=document.getElementById("schermo").clientWidth;
 alt_sprite=quadrato.offsetTop;
 lar_sprite=quadrato.offsetLeft;
 barraSprite=document.getElementById("barra");
+quadrato=document.getElementById("quadrato");
+cuore=document.getElementById("cuore");
 step=0;
 step=schermox+30;
 jumprange=80;
 jumpspeed=1;
 altezzabase=180;
+var rotazione=0;
+score=0;
 function movimento(){
-    quadrato=document.getElementById("quadrato");
+    
     alt_sprite=quadrato.offsetTop;
     lar_sprite=quadrato.offsetLeft;
     lar_barra=barraSprite.offsetLeft;
     alt_barra=barraSprite.offsetTop;
+    alt_cuore=cuore.offsetTop;
+    lar_cuore=cuore.offsetLeft;
     deltaTop=alt_sprite-schermoy;
     deltaLeft=lar_sprite-schermox;
-    //console.log("|MonitorX:"+schermox+"|MonitorY:"+schermoy+"|XSprite:"+lar_sprite,"|YSprite:"+alt_sprite);
     
+    //console.log("|MonitorX:"+schermox+"|MonitorY:"+schermoy+"|XSprite:"+lar_sprite,"|YSprite:"+alt_sprite);
     document.addEventListener("keydown", comandi);
-    if(lar_sprite == lar_barra && alt_sprite == alt_barra ){alert("Game Over!")}
-    barra();
+    document.getElementById("score-text").innerHTML = "Score:"+score;
+    if(isColliding(quadrato,barraSprite)){
+        alert("Game Over!") ;
+        clearInterval(gameengine);
+    }else{barra();}
+    
 }
 function barra(){
+    
     barraSprite.style.visibility="visible";
     step-=1
     barraSprite.style.left=step+"px";
     if(barraSprite.offsetLeft == 0)
     {
+        score+=1;
         barraSprite.style.visibility="hidden";
         barraSprite.style.left=schermox+50;
         step=schermox+49;
@@ -42,7 +54,10 @@ function comandi(event){
     }
 
 }
-function salto(){   
+function salto(){ 
+    rotazione+=15;
+    if(rotazione <= 180){quadrato.style.transform ="rotate("+rotazione+"deg)";}else{rotazione=0;}
+    console.log("R"+rotazione);
     jumpspeed=alt_sprite;
     jumpspeed-=4;
     quadrato.style.top=jumpspeed+"px";  
@@ -50,13 +65,32 @@ function salto(){
 
     if(alt_sprite <= altezzabase-jumprange){
         clearInterval(Isalto);
-        Ibase=setInterval(base,18)
+        Ibase=setInterval(base,5)
     }
 }
 function base(){
             jumpspeed=alt_sprite;
-            jumpspeed+=4;
+            jumpspeed+=1;
             quadrato.style.top=jumpspeed+"px";  
             console.log(alt_sprite,jumprange,altezzabase,altezzabase-jumprange)
-            if(alt_sprite == 180){clearInterval(Ibase);}
+            if(alt_sprite == alt_barra-20){clearInterval(Ibase);}
 }
+let isColliding = function (div1, div2) {
+
+    //let d1Offset = div1.offset();
+    let d1offtop   = div1.offsetTop;
+    let d1offleft   = div1.offsetLeft;
+    let d1Height=div1.clientHeight;
+    let d1Width=div1.clientWidth;
+    let d1Top = div1.offsetTop + d1Height;
+    let d1Left = div1.offsetLeft + d1Width;
+
+    let d2offtop   = div2.offsetTop;
+    let d2offleft   = div2.offsetLeft;
+    let d2Height=div2.clientHeight;
+    let d2Width=div2.clientWidth;
+    let d2Top = div2.offsetTop + d2Height;
+    let d2Left = div2.offsetLeft + d2Width;
+
+    return !(d1Top < div2.offsetTop || div1.offsetTop > d2Top || d1Left < div2.offsetLeft || div1.offsetLeft > d2Left);
+};
