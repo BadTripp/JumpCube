90694c098800c6ae8024a8921a9bc687a7b5fd04
BadTripp <renato.turetta00@gmail.com>
2022-03-24 13:56:05 +0100

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,54 @@
+setInterval(movimento,5)
+schermoy=document.getElementById("schermo").clientHeight;
+schermox=document.getElementById("schermo").clientWidth;
+alt_sprite=quadrato.offsetTop;
+lar_sprite=quadrato.offsetLeft;
+barraSprite=document.getElementById("barra");
+step=0;
+step=schermox+30;
+jumprange=50;
+jumpspeed=1;
+function movimento(){
+    quadrato=document.getElementById("quadrato");
+    alt_sprite=quadrato.offsetTop;
+    lar_sprite=quadrato.offsetLeft;
+    deltaTop=alt_sprite-schermoy;
+    deltaLeft=lar_sprite-schermox;
+    //console.log("|MonitorX:"+schermox+"|MonitorY:"+schermoy+"|XSprite:"+lar_sprite,"|YSprite:"+alt_sprite);
+    
+    document.addEventListener("keydown", comandi);
+    barra();
+}
+function barra(){
+    barraSprite.style.visibility="visible";
+    step-=1
+    barraSprite.style.left=step+"px";
+    if(barraSprite.offsetLeft == 0)
+    {
+        barraSprite.style.visibility="hidden";
+        barraSprite.style.left=schermox+50;
+        step=schermox+49;
+    }
+}
+function comandi(event){
+    var keyID = event.keyCode;
+    
+    c=0;
+    if(keyID == 32){
+        
+           Isalto= setInterval(salto,90);
+           console.log(alt_sprite);
+            
+            
+        
+        
+    }
+
+}
+function salto(){
+    jumpspeed+=1;
+    jumpspeed=alt_sprite+jumpspeed
+    console.log(jumpspeed);
+            quadrato.style.top=+"rx";
+    if(alt_sprite <= 188+50){clearInterval(Isalto);}
+}
